f61320c9ce84c0382623e074ed40082e2cdccd95
mihaiconstantin <mihai.a.constantin@gmail.com>
2020-12-20 13:12:39 +0100

    Added basic implementation for the GGM model

---
+++ R/model.R
@@ -0,0 +1,76 @@
+# ------------------------------------------------------------------------------
+# # # # Model functions.
+# ------------------------------------------------------------------------------
+
+# Contains environments with models that can be used with the algorithm.
+
+# ------------------------------------------------------------------------------
+# # # The GGM model.
+# ------------------------------------------------------------------------------
+
+
+#' @title Create GGM class.
+#' @export
+ggm <- new.env()
+
+
+# Random mechanism.
+ggm$create <- function(...) {
+    return(bootnet::genGGM(...))
+}
+
+
+# Generate data.
+ggm$generate <- function(n, model, levels = 5) {
+    # To correlations.
+    model <- cov2cor(solve(diag(ncol(model)) - model))
+
+    # Sample data.
+    data <- mvtnorm::rmvnorm(n, sigma = model)
+
+    # Split the data into item steps.
+    for (i in seq_len(ncol(data))) {
+        data[, i] <- as.numeric(cut(data[, i], sort(c(-Inf, rnorm(levels - 1), Inf))))
+    }
+
+    return(data)
+}
+
+
+# Estimator.
+ggm$estimate <- function(data, ...) {
+    return(
+        suppressWarnings(
+            suppressMessages(
+                bootnet::estimateNetwork(data, default = "EBICglasso", verbose = FALSE, ...)$graph
+            )
+        )
+    )
+}
+
+
+# Compute outcomes.
+ggm$evaluate <- function(true, esti) {
+    # Get the true and estimated edges.
+    true <- true[upper.tri(true)]
+    esti <- esti[upper.tri(esti)]
+
+    # True/ false | positive/ negative rates.
+    TP <- sum(true != 0 & esti != 0)
+    FP <- sum(true == 0 & esti != 0)
+    TN <- sum(true == 0 & esti == 0)
+    FN <- sum(true != 0 & esti == 0)
+
+    # Compute indicators based on the rates.
+    sen <- TP / (TP + FN)
+    spe <- TN / (TN + FP)
+    mcc <- (TP * TN - FP * FN) / sqrt((TP + FP) * (TP + FN) * (TN + FP) * (TN + FN))
+    rho <- ifelse((length(true) == length(esti)) && ((var(true) != 0) && (var(esti) != 0)), cor(true, esti), NA)
+
+    return(list(
+        sen = sen,
+        spe = spe,
+        mcc = mcc,
+        rho = rho
+    ))
+}
