e068cf04a6cac8d2b5110ca4d503f26e104ea935
mihaiconstantin <mihai.a.constantin@gmail.com>
2020-12-20 13:12:22 +0100

    Added package file

---
+++ R/zzz.R
@@ -0,0 +1,18 @@
+#' \code{splower}: Flexible Power Analysis for Network Models.
+#'
+#' The \code{splower} Run power analysis for various cross-sectional network models.
+#'
+#' @import splines2
+#' @import nnls
+#' @import bootnet
+#' @import progress
+#' @import qgraph
+#' @import ggplot2
+#' @import gridExtra
+#' @import reshape2
+#' @import shiny
+#' @import R6
+#'
+#' @docType package
+#' @name splower
+"_PACKAGE"
