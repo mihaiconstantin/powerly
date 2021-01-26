83f6a0b1567b75740868607523e31c417a876849
mihaiconstantin <mihai.a.constantin@gmail.com>
2021-01-26 18:03:49 +0100

    Changes function arguments for consistency

--- R/validation.R
+++ R/validation.R
@@ -1,11 +1,11 @@
 #' @title Validate recommendation.
 #' @export
-validate.recommendation <- function(model, recommendation, replications = 100, measure = "sen", target = .8, statistic = "power", ..., verbose = TRUE) {
+validate.recommendation <- function(model, recommendation, replications = 100, performance.measure = "sen", performance.measure.target = .8, statistic.definition = "power", ..., verbose = TRUE) {
     # User feedback.
     if(verbose) cat("Starting validation...", "\n")
 
     # Validate.
-    validation <- run.step.1(model = model, selected.sample.sizes = recommendation, replications = replications,  performance.measure = measure, performance.measure.target = target, statistic.definition = statistic, ..., verbose = verbose)
+    validation <- run.step.1(model = model, selected.sample.sizes = recommendation, replications = replications,  performance.measure = performance.measure, performance.measure.target = performance.measure.target, statistic.definition = statistic.definition, ..., verbose = verbose)
 
     # Add class.
     class(validation) <- "validation"
