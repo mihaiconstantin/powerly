c50aab97cc0025147ebe17dd0695be594a75dc80
mihaiconstantin <mihai.a.constantin@gmail.com>
2021-01-27 10:44:12 +0100

    Updated package documentation

--- R/zzz.R
+++ R/zzz.R
@@ -1,17 +1,14 @@
-#' \code{splower}: Flexible Power Analysis for Network Models.
+#' \code{splower}: Sample Size Analysis for Psychological Network Models.
 #'
-#' The \code{splower} Run power analysis for various cross-sectional network models.
+#' The \code{splower} Run sample size analysis for cross-sectional network models.
+#' 
+#' The package is under active development.
+#' First major release is expected on 01.31.2021.
 #'
 #' @import splines2
 #' @import nnls
 #' @import bootnet
 #' @import progress
-#' @import qgraph
-#' @import ggplot2
-#' @import gridExtra
-#' @import reshape2
-#' @import shiny
-#' @import R6
 #'
 #' @docType package
 #' @name splower
