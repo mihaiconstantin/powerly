de7bc77aa35fdb38c22a2f44955f3326fab0d456
mihaiconstantin <mihai@mihaiconstantin.com>
2021-06-18 21:00:34 +0200

    Renamed package file

---
+++ R/powerly-package.R
@@ -0,0 +1,37 @@
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+#                                            _            #
+#                                           | |           #
+#   _ __     ___   __      __   ___   _ __  | |  _   _    #
+#  | '_ \   / _ \  \ \ /\ / /  / _ \ | '__| | | | | | |   #
+#  | |_) | | (_) |  \ V  V /  |  __/ | |    | | | |_| |   #
+#  | .__/   \___/    \_/\_/    \___| |_|    |_|  \__, |   #
+#  | |                                            __/ |   #
+#  |_|                                           |___/    #
+#                                                         #
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+# Author: Mihai A. Constantin                             #
+# Contact: mihai@mihaiconstantin.com                      #
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+
+#' @include logo.R
+
+#' \code{powerly}: Sample size analysis for complex models.
+#'
+#' # TODO: Add documentation.
+#'
+#' @import R6
+#' @import splines2
+#' @import nnls
+#' @import bootnet
+#' @import qgraph
+#' @import parallel
+#'
+#' @docType package
+#' @name powerly
+"_PACKAGE"
+
+# On package attach or load.
+.onAttach <- function(libname, pkgname) {
+    # Print the logo.
+    cat(LOGO, "\n")
+}
