f84d0eaa3d4589d2d6e220a786e763300324f8d3
mihaiconstantin <mihai@mihaiconstantin.com>
2021-06-19 00:49:28 +0200

    Simplified implementation for GGM model

--- R/GgmModel.R
+++ R/GgmModel.R
@@ -3,37 +3,6 @@
 GgmModel <- R6::R6Class("GgmModel",
     inherit = Model,
 
-    private = list(
-        .tp = NULL,
-        .fp = NULL,
-        .tn = NULL,
-        .fn = NULL,
-
-        .compute_rates = function(true_parameters, estimated_parameters) {
-            # Get the true and estimated edges.
-            true <- true_parameters[upper.tri(true_parameters)]
-            esti <- estimated_parameters[upper.tri(estimated_parameters)]
-
-            # True/ false | positive/ negative rates.
-            private$.tp <- sum(true != 0 & esti != 0)
-            private$.fp <- sum(true == 0 & esti != 0)
-            private$.tn <- sum(true == 0 & esti == 0)
-            private$.fn <- sum(true != 0 & esti == 0)
-        },
-
-        .sen = function() {
-            return(private$.tp / (private$.tp + private$.fn))
-        },
-
-        .spe = function() {
-            return(private$.tn / (private$.tn + private$.fp))
-        },
-
-        .mcc = function() {
-            return((private$.tp * private$.tn - private$.fp * private$.fn) / sqrt((private$.tp + private$.fp) * (private$.tp + private$.fn) * (private$.tn + private$.fp) * (private$.tn + private$.fn)))
-        }
-    ),
-
     public = list(
         create = function(nodes, density) {
             return(bootnet::genGGM(nodes, p = density, propPositive = .5, graph = "random"))
@@ -59,17 +28,26 @@ GgmModel <- R6::R6Class("GgmModel",
         },
 
         evaluate = function(true_parameters, estimated_parameters, measure) {
-            # Compute rates because they are required for most of all other measures.
-            private$.compute_rates(true_parameters, estimated_parameters)
+            # Extract the true and estimated parameters from the weights matrices.
+            true <- true_parameters[upper.tri(true_parameters)]
+            esti <- estimated_parameters[upper.tri(estimated_parameters)]
+
+            # Compute true/ false | positive/ negative rates.
+            tp <- sum(true != 0 & esti != 0)
+            fp <- sum(true == 0 & esti != 0)
+            tn <- sum(true == 0 & esti == 0)
+            fn <- sum(true != 0 & esti == 0)
 
             # Compute correct measure.
-            return(switch(measure,
-                sen = private$.sen(),
-                spe = private$.spe(),
-                mcc = private$.mcc(),
-                rho = ifelse(length(true_parameters) == length(estimated_parameters), cor(true_parameters, estimated_parameters, use = "complete.obs"), NA),
-                stop(.__ERRORS__$not_developed)
-            ))
+            return(
+                switch(measure,
+                    sen = tp / (tp + fn),
+                    spe = tn / (tn + fp),
+                    mcc = (tp * tn - fp * fn) / sqrt((tp + fp) * (tp + fn) * (tn + fp) * (tn + fn)),
+                    rho = ifelse(length(true) == length(esti), cor(true, esti, use = "complete.obs"), NA),
+                    stop(.__ERRORS__$not_developed)
+                )
+            )
         }
     )
 )
