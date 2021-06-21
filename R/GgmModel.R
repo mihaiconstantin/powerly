51d8ace58a406e3c5adc0da80e697cd5c7386a52
mihaiconstantin <mihai@mihaiconstantin.com>
2021-06-21 09:07:00 +0200

    Implemented own estimator for 'GgmModel'

--- R/GgmModel.R
+++ R/GgmModel.R
@@ -23,8 +23,49 @@ GgmModel <- R6::R6Class("GgmModel",
             return(data)
         },
 
-        estimate = function(data) {
-            return(bootnet::estimateNetwork(data, default = "EBICglasso", verbose = FALSE)$graph)
+        estimate = function(data, n_lambda = 100, lambda_min_ratio = .01, gamma = 0.5) {
+            # Compute correlation matrix (i.e., that's what 'qgraph' uses).
+            S <- cor(data)
+
+            # Compute the number of respondents.
+            n <- nrow(data)
+
+            # Compute the number of variables.
+            p <- nrow(S)
+
+            # Compute range of tunning parameter to be tried.
+            lambda_max = max(max(S - diag(p)), -min(S - diag(p)))
+            lambda_min = lambda_min_ratio * lambda_max
+            lambda = exp(seq(log(lambda_min), log(lambda_max), length = n_lambda))
+
+            # Compute 'glasso' path for the corresponding tunning parameters.
+            precision_matrices <- glasso::glassopath(S, lambda, trace = 0, penalize.diagonal = FALSE)
+
+            # Compute EBIC values for the 'glasso' path matrices.
+            ebic_values <- sapply(seq_along(lambda), function(i) {
+                # Extract the current precision matrix for easier access.
+                K <- precision_matrices$wi[, , i]
+
+                # Compute log likelihood.
+                L <- n / 2 * (log(det(K)) - sum(diag(K %*% S)))
+
+                # Compute the edge set.
+                E <- sum(K[lower.tri(K, diag = FALSE)] != 0)
+
+                # Compute the EBIC.
+                return(-2 * L + E * log(n) + 4 * E * gamma * log(p))
+            })
+
+            # Get precision matrix that minimizes the EBIC.
+            K <- precision_matrices$wi[, , which.min(ebic_values)]
+
+            # Standardize precision coefficients to get partial correlation coefficients.
+            W <- -cov2cor(K)
+
+            # Set diagonal to 0.
+            diag(W) <- 0
+
+            return(as.matrix(Matrix::forceSymmetric(W)))
         },
 
         evaluate = function(true_parameters, estimated_parameters, measure) {
