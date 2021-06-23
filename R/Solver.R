2420aca3b10c3d6ec6ca6225c30885f4c8181362
mihaiconstantin <mihai@mihaiconstantin.com>
2021-06-23 16:24:47 +0200

    Feat (solver): add abstract 'setup' method

--- R/Solver.R
+++ R/Solver.R
@@ -1,5 +1,10 @@
 Solver <- R6::R6Class("Solver",
     public = list(
+        # Setup solver.
+        setup = function(basis, y, increasing, ...) {
+            stop(.__ERRORS__$not_implemented)
+        },
+
         # Solve the problem give the data.
         solve = function(...) {
             stop(.__ERRORS__$not_implemented)
