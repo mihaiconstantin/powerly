adfdbabfbdf84ff3d9695482311e549da175b2a7
mihaiconstantin <mihai@mihaiconstantin.com>
2021-06-23 14:42:59 +0200

    Feat (solver): add solver using 'quadprog' implementation
    The default solver until now was based on the 'osqp' package. However,
    this solver was not compatible with parallelization backends (e.g.,
    'parallel' package; see https://stackoverflow.com/q/68095114/5252007).
    The 'QuadprogSolver' comes as an alternative to the 'OsqpSolver'.

---
+++ R/QuadprogSolver.R
@@ -0,0 +1,100 @@
+QuadprogSolver <- R6::R6Class("QuadprogSolver",
+    private = list(
+        .basis = NULL,
+        .y = NULL,
+        .increasing = NULL,
+
+        .n = NULL,
+        .updated = FALSE,
+
+        # Solver equation inputs.
+        .d_mat = NULL,
+        .d_vec = NULL,
+        .a_mat = NULL,
+        .b_vec = NULL,
+
+        # Create matrices used by the solver.
+        .create_matrices = function() {
+            private$.d_mat <- crossprod(private$.basis$matrix, private$.basis$matrix)
+            private$.d_vec <- crossprod(private$.basis$matrix, private$.y)
+        },
+
+        # Set constraints matrix.
+        .set_constraints = function() {
+            # Apply equality constraints as necessary.
+            if (private$.basis$monotone) {
+                if (private$.increasing) {
+                    # Non-negativity constraints (i.e., used for non-decreasing I-Splines).
+                    a_mat <- diag(1, private$.n)
+                } else {
+                    # Non-positivity constraints (i.e., used for non-increasing I-Splines).
+                    a_mat <- diag(-1, private$.n)
+                }
+                # For I-Splines, release the first parameter.
+                 a_mat[1, 1] <- 0
+            } else {
+                # No constraints (i.e., used for B-Splines).
+                a_mat <- diag(0, private$.n)
+            }
+
+            # Set the constraints matrix.
+            private$.a_mat <- a_mat
+        },
+
+        # Set bounds for the constraints matrix.
+        .set_bounds = function() {
+            private$.b_vec <- rep(0, private$.n)
+        },
+
+        # Core solver method.
+        .solve = function() {
+            return(
+                quadprog::solve.QP(
+                    Dmat = private$.d_mat,
+                    dvec = private$.d_vec,
+                    Amat = private$.a_mat,
+                    bvec = private$.b_vec,
+                    meq = 0
+                )$solution
+            )
+        }
+    ),
+
+    public = list(
+        initialize = function(basis, y, increasing = NULL) {
+            # Set input.
+            private$.basis <- basis
+            private$.y <- y
+            private$.increasing <- increasing
+
+            # Compute number of basis functions.
+            private$.n <- ncol(basis$matrix)
+
+            # Prepare the solver.
+            private$.create_matrices()
+            private$.set_constraints()
+            private$.set_bounds()
+        },
+
+        solve = function() {
+            # Return the solution.
+            return(private$.solve())
+        },
+
+        solve_update = function(y_new) {
+            # Update the 'd' vector.
+            private$.d_vec <- crossprod(private$.basis$matrix, y_new)
+
+            # Indicate that an update ocurred.
+            private$.updated = TRUE
+
+            # Solve and return the updated solution.
+            return(private$.solve())
+        }
+    ),
+
+    active = list(
+        increasing = function() { return(private$.increasing) },
+        updated = function() { return(private$.updated) }
+    )
+)
