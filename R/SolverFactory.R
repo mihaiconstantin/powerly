409402f1b68f2e2698d48a856571e8a25f94f936
mihaiconstantin <mihai@mihaiconstantin.com>
2021-06-23 16:44:18 +0200

    Refactor (solver): update factory to respect abstract 'Solver'

--- R/SolverFactory.R
+++ R/SolverFactory.R
@@ -2,11 +2,11 @@
 
 SolverFactory <- R6::R6Class("SolverFactory",
     public = list(
-        get_solver = function(type, ...) {
+        get_solver = function(type) {
             return(
                 switch(type,
-                    osqp = OsqpSolver$new(...),
-                    quadprog = QuadprogSolver$new(...),
+                    osqp = OsqpSolver$new(),
+                    quadprog = QuadprogSolver$new(),
                     stop(.__ERRORS__$not_developed)
                 )
             )
