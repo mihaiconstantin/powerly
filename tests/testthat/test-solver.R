fe4847f950db63d0c347f57f0bfa4b236c11d8b8
mihaiconstantin <mihai@mihaiconstantin.com>
2021-06-23 18:25:26 +0200

    Test (solver): update tests for new implementation

--- tests/testthat/test-solver.R
+++ tests/testthat/test-solver.R
@@ -1,6 +1,6 @@
-# Testing the 'Solver' class.
+# Testing concrete implementations of the 'Solver' class.
 
-test_that("'Solver' sets correct constraints for monotone non-decreasing spline", {
+test_that("'Solver' implementations set correct constraints for monotone non-decreasing spline", {
     # Data.
     x <- 1:10
     y <- c(-.5, .8, .6, 1, .3, 1, 1, 1, 1, .5)
@@ -9,16 +9,30 @@ test_that("'Solver' sets correct constraints for monotone non-decreasing spline"
     ispline <- Basis$new(x, df = 0, monotone = TRUE)
     n <- ncol(ispline$matrix)
 
-    # Create solver.
-    solver <- Solver$new(ispline, y, increasing = TRUE)
+    # Create solvers.
+    osqp <- OsqpSolver$new()
+    qp <- QuadprogSolver$new()
 
-    # Test.
-    expect_equal(solver$lower_bounds, c(-Inf, rep(0, n - 1)))
-    expect_equal(solver$upper_bounds, rep(Inf, n))
+    # Setup solvers.
+    osqp$setup(ispline, y, increasing = TRUE)
+    qp$setup(ispline, y, increasing = TRUE)
+
+    # Test bounds for 'OsqpSolver'.
+    expect_equal(osqp$.__enclos_env__$private$.lower, c(-Inf, rep(0, n - 1)))
+    expect_equal(osqp$.__enclos_env__$private$.upper, rep(Inf, n))
+
+    # Constraints for 'QuadprogSolver'.
+    b_vec <- rep(0, n)
+    a_mat <- diag(1, n)
+    a_mat[1, 1] <- 0
+
+    # Test constraints for 'QuadprogSolver'.
+    expect_equal(qp$.__enclos_env__$private$.b_vec, b_vec)
+    expect_equal(qp$.__enclos_env__$private$.a_mat, a_mat)
 })
 
 
-test_that("'Solver' sets correct constraints for monotone non-increasing spline", {
+test_that("'Solver' implementations set correct constraints for monotone non-increasing spline", {
     # Data.
     x <- 1:10
     y <- c(-.5, .8, .6, 1, .3, 1, 1, 1, 1, .5)
@@ -27,16 +41,30 @@ test_that("'Solver' sets correct constraints for monotone non-increasing spline"
     ispline <- Basis$new(x, df = 0, monotone = TRUE)
     n <- ncol(ispline$matrix)
 
-    # Create solver.
-    solver <- Solver$new(ispline, y, increasing = FALSE)
+    # Create solvers.
+    osqp <- OsqpSolver$new()
+    qp <- QuadprogSolver$new()
 
-    # Test.
-    expect_equal(solver$lower_bounds, rep(-Inf, n))
-    expect_equal(solver$upper_bounds, c(Inf, rep(0, n - 1)))
+    # Setup solvers.
+    osqp$setup(ispline, y, increasing = FALSE)
+    qp$setup(ispline, y, increasing = FALSE)
+
+    # Test bounds for 'OsqpSolver'.
+    expect_equal(osqp$.__enclos_env__$private$.lower, rep(-Inf, n))
+    expect_equal(osqp$.__enclos_env__$private$.upper, c(Inf, rep(0, n - 1)))
+
+    # Constraints for 'QuadprogSolver'.
+    b_vec <- rep(0, n)
+    a_mat <- diag(-1, n)
+    a_mat[1, 1] <- 0
+
+    # Test constraints for 'QuadprogSolver'.
+    expect_equal(qp$.__enclos_env__$private$.b_vec, b_vec)
+    expect_equal(qp$.__enclos_env__$private$.a_mat, a_mat)
 })
 
 
-test_that("'Solver' sets correct constraints for non-monotone spline", {
+test_that("'Solver' implementations set correct constraints for non-monotone spline", {
     # Data.
     x <- 1:10
     y <- c(-.5, .8, .6, 1, .3, 1, 1, 1, 1, .5)
@@ -45,16 +73,29 @@ test_that("'Solver' sets correct constraints for non-monotone spline", {
     bspline <- Basis$new(x, df = 0, monotone = FALSE)
     n <- ncol(bspline$matrix)
 
-    # Create solver.
-    solver <- Solver$new(bspline, y)
+    # Create solvers.
+    osqp <- OsqpSolver$new()
+    qp <- QuadprogSolver$new()
 
-    # Test.
-    expect_equal(solver$lower_bounds, rep(-Inf, n))
-    expect_equal(solver$upper_bounds, rep(Inf, n))
+    # Setup solvers.
+    osqp$setup(bspline, y)
+    qp$setup(bspline, y)
+
+    # Test bounds for 'OsqpSolver'.
+    expect_equal(osqp$.__enclos_env__$private$.lower, rep(-Inf, n))
+    expect_equal(osqp$.__enclos_env__$private$.upper, rep(Inf, n))
+
+    # Constraints for 'QuadprogSolver'.
+    b_vec <- rep(0, n)
+    a_mat <- diag(0, n)
+
+    # Test constraints for 'QuadprogSolver'.
+    expect_equal(qp$.__enclos_env__$private$.b_vec, b_vec)
+    expect_equal(qp$.__enclos_env__$private$.a_mat, a_mat)
 })
 
 
-test_that("'Solver' gives correct solution for monotone non-decreasing spline", {
+test_that("'Solver' implementations give correct solution for monotone non-decreasing spline", {
     # Data.
     x <- 1:10
     y <- c(-0.2, 0.3, 0.5, 0.7, 0.6, 1, 0.9, 1, 1, 1)
@@ -63,20 +104,37 @@ test_that("'Solver' gives correct solution for monotone non-decreasing spline", 
     ispline <- Basis$new(x, df = 0, monotone = TRUE)
     n <- ncol(ispline$matrix)
 
-    # Solve using solver.
-    solver_alpha <- Solver$new(ispline, y, increasing = TRUE)$solve()
+    # Create solvers.
+    osqp <- OsqpSolver$new()
+    qp <- QuadprogSolver$new()
+
+    # Setup solvers.
+    osqp$setup(ispline, y, increasing = TRUE)
+    qp$setup(ispline, y, increasing = TRUE)
+
+    # Solve using own implementations.
+    osqp_impl_alpha <- osqp$solve()
+    qp_impl_alpha <- qp$solve()
 
     # Solve using 'quadprog'.
     a_mat <- diag(1, n)
     a_mat[1, 1] <- 0
     b_vec <- rep(0, n)
-    quadprog_alpha <- solve_qp(ispline$matrix, y, a_mat, b_vec)
+    qp_alpha <- solve_qp(ispline$matrix, y, a_mat, b_vec)
 
-    expect_equal(solver_alpha, quadprog_alpha)
+    # Solve using 'osqp'.
+    osqp_alpha <- solve_osqp(ispline$matrix, y, c(-Inf, rep(0, n - 1)), rep(Inf, n))
+
+    # Each implementation should be equal with its counterpart.
+    expect_equal(osqp_impl_alpha, osqp_alpha)
+    expect_equal(qp_impl_alpha, qp_alpha)
+
+    # The implementations should also give the same solution.
+    expect_equal(osqp_impl_alpha, qp_impl_alpha)
 })
 
 
-test_that("'Solver' gives correct solution for monotone non-increasing spline", {
+test_that("'Solver' implementations give correct solution for monotone non-increasing spline", {
     # Data.
     x <- 1:10
     y <- rev(c(-0.2, 0.3, 0.5, 0.7, 0.6, 1, 0.9, 1, 1, 1))
@@ -85,20 +143,37 @@ test_that("'Solver' gives correct solution for monotone non-increasing spline", 
     ispline <- Basis$new(x, df = 0, monotone = TRUE)
     n <- ncol(ispline$matrix)
 
-    # Solve using solver.
-    solver_alpha <- Solver$new(ispline, y, increasing = FALSE)$solve()
+    # Create solvers.
+    osqp <- OsqpSolver$new()
+    qp <- QuadprogSolver$new()
+
+    # Setup solvers.
+    osqp$setup(ispline, y, increasing = FALSE)
+    qp$setup(ispline, y, increasing = FALSE)
+
+    # Solve using own implementations.
+    osqp_impl_alpha <- osqp$solve()
+    qp_impl_alpha <- qp$solve()
 
     # Solve using 'quadprog'.
     a_mat <- diag(-1, n)
     a_mat[1, 1] <- 0
     b_vec <- rep(0, n)
-    quadprog_alpha <- solve_qp(ispline$matrix, y, a_mat, b_vec)
+    qp_alpha <- solve_qp(ispline$matrix, y, a_mat, b_vec)
 
-    expect_equal(solver_alpha, quadprog_alpha)
+    # Solve using 'osqp'.
+    osqp_alpha <- solve_osqp(ispline$matrix, y, rep(-Inf, n), c(Inf, rep(0, n - 1)))
+
+    # Each implementation should be equal with its counterpart.
+    expect_equal(osqp_impl_alpha, osqp_alpha)
+    expect_equal(qp_impl_alpha, qp_alpha)
+
+    # The implementations should also give the same solution.
+    expect_equal(osqp_impl_alpha, qp_impl_alpha)
 })
 
 
-test_that("'Solver' gives correct solution for non-monotone spline", {
+test_that("'Solver' implementations give correct solution for non-monotone spline", {
     # Data.
     x <- 1:10
     y <- c(-.5, .8, .6, 1, .3, 1, 1, 1, 1, .5)
@@ -106,18 +181,28 @@ test_that("'Solver' gives correct solution for non-monotone spline", {
     # Create spline.
     bspline <- Basis$new(x, df = 0, monotone = FALSE)
 
-    # Solve using solver.
-    solver_alpha <- Solver$new(bspline, y)$solve()
+    # Create solvers.
+    osqp <- OsqpSolver$new()
+    qp <- QuadprogSolver$new()
+
+    # Setup solvers.
+    osqp$setup(bspline, y)
+    qp$setup(bspline, y)
+
+    # Solve using own implementations.
+    osqp_impl_alpha <- osqp$solve()
+    qp_impl_alpha <- qp$solve()
 
     # Solve using 'lm'.
     lm_alpha <- as.numeric(lm.fit(bspline$matrix, y)$coefficients)
 
     # Test.
-    expect_equal(solver_alpha, lm_alpha)
+    expect_equal(osqp_impl_alpha, lm_alpha)
+    expect_equal(qp_impl_alpha, lm_alpha)
 })
 
 
-test_that("'Solver' gives correct solution for updated statistics", {
+test_that("'Solver' implementations gives correct solution for updated statistics", {
     # Data.
     x <- 1:10
     y <- c(-0.2, 0.3, 0.5, 0.7, 0.6, 1, 0.9, 1, 1, 1)
@@ -126,34 +211,41 @@ test_that("'Solver' gives correct solution for updated statistics", {
     ispline <- Basis$new(x, df = 0, monotone = TRUE)
     n <- ncol(ispline$matrix)
 
-    # Create solver.
-    solver <- Solver$new(ispline, y, increasing = TRUE)
+    # Create solvers.
+    osqp <- OsqpSolver$new()
+    qp <- QuadprogSolver$new()
 
-    # Solve the original problem to create the model.
-    solver$solve()
+    # Setup solvers.
+    osqp$setup(ispline, y, increasing = TRUE)
+    qp$setup(ispline, y, increasing = TRUE)
+
+    # Solve first time with original data.
+    osqp$solve()
+    qp$solve()
 
     # Create new data to update the solver.
     y_new <- sample(y, length(y), TRUE)
 
-    # Update solver and solve problem.
-    solver_alpha <- solver$solve.update(y_new)
+    # Update solvers and solve problem.
+    osqp_impl_alpha <- osqp$solve_update(y_new)
+    qp_impl_alpha <- qp$solve_update(y_new)
 
     # Solve problem with new data using 'osqp' helper.
-    osqp_alpha <- solve_osqp(ispline$matrix, y_new, solver$lower_bounds, solver$upper_bounds)
+    osqp_alpha <- solve_osqp(ispline$matrix, y_new, osqp$.__enclos_env__$private$.lower, osqp$.__enclos_env__$private$.upper)
 
     # Solve problem with new data using 'quadprog' helper.
-    a_mat <- diag(1, n)
-    a_mat[1, 1] <- 0
-    b_vec <- rep(0, n)
-    quadprog_alpha <- solve_qp(ispline$matrix, y_new, a_mat, b_vec)
+    qp_alpha <- solve_qp(ispline$matrix, y_new, qp$.__enclos_env__$private$.a_mat, qp$.__enclos_env__$private$.b_vec)
 
-    # Tests.
-    expect_equal(solver_alpha, osqp_alpha)
-    expect_equal(solver_alpha, quadprog_alpha)
+    # The solver implementations should agree with their counterpart helpers.
+    expect_equal(osqp_impl_alpha, osqp_alpha)
+    expect_equal(qp_impl_alpha, qp_alpha)
+
+    # The solver implementations should give the same solution.
+    expect_equal(osqp_impl_alpha, qp_impl_alpha)
 })
 
 
-test_that("'Solver' throws error if trying to update non-model", {
+test_that("'Solver' implementations still give original solution after solving with updated data", {
     # Data.
     x <- 1:10
     y <- c(-0.2, 0.3, 0.5, 0.7, 0.6, 1, 0.9, 1, 1, 1)
@@ -161,12 +253,26 @@ test_that("'Solver' throws error if trying to update non-model", {
     # Create spline.
     ispline <- Basis$new(x, df = 0, monotone = TRUE)
 
-    # Create solver.
-    solver <- Solver$new(ispline, y, increasing = TRUE)
+    # Create solvers.
+    osqp <- OsqpSolver$new()
+    qp <- QuadprogSolver$new()
+
+    # Setup solvers.
+    osqp$setup(ispline, y, increasing = TRUE)
+    qp$setup(ispline, y, increasing = TRUE)
+
+    # Solve first time with original data.
+    osqp_first_alpha <- osqp$solve()
+    qp_first_alpha <- qp$solve()
 
     # Create new data to update the solver.
     y_new <- sample(y, length(y), TRUE)
 
-    # Update solver and solve problem without having called '$solve()' previously.
-    testthat::expect_error(solver$solve.update(y_new))
+    # Solve with updated data.
+    osqp$solve_update(y_new)
+    qp$solve_update(y_new)
+
+    # Solve again and expect to recover the original solution.
+    expect_equal(osqp_first_alpha, osqp$solve())
+    expect_equal(qp_first_alpha, qp$solve())
 })
