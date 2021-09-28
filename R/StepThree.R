e745aceacf099bfeb9b6f72173d8f334e69c7316
mihaiconstantin <mihai@mihaiconstantin.com>
2021-09-28 06:12:25 +0200

    Feat: add `ggplot2` plots for `StepThree`

--- R/StepThree.R
+++ R/StepThree.R
@@ -255,200 +255,157 @@ StepThree <- R6::R6Class("StepThree",
         },
 
         # Plot.
-        plot = function() {
-            # Revert the changes on exit.
-            on.exit({
-                # Restore layout.
-                layout(1:1)
-
-                # Restore margins to default.
-                par(mar = c(5.1, 4.1, 4.1, 2.1))
-            })
-
-            # Layout.
-            layout(matrix(c(1, 1, 2, 3), 2, 2, byrow = TRUE))
-
-            # Adjust margins for layout.
-            par(mar = c(5.1, 4.1, 4.1, 2.1) + 1)
-
-            # Plot spline and confidence intervals.
-            plot(
-                NULL,
-                xlim = c(min(private$.step_2$step_1$range$partition), max(private$.step_2$step_1$range$partition)),
-                ylim = c(min(private$.ci) - 0.2, max(private$.ci) + 0.2),
-                xlab = "",
-                ylab = "",
-                xaxt = "n",
-                yaxt = "n",
-                cex = 1
-            )
-            title(
-                main = paste0("Bootstrap CI | Distance: ", private$.samples[self$upper_ci_string] - private$.samples[self$lower_ci_string], " sample sizes"),
-                ylab = paste0("Value for statistic '", toupper(sub("Statistic", "", class(private$.step_2$step_1$statistic)[1])), "'"),
-                cex.lab = 1,
-                cex.main = 1
-            )
-            title(
-                xlab = "Sample size",
-                cex.lab = 1,
-                line = 4
-            )
-            axis(
-                side = 1,
-                at = private$.step_2$step_1$range$partition,
-                las = 2,
-                cex.axis = .9
-            )
-            axis(
-                at = round(seq(min(private$.ci), max(private$.ci), length.out = 10), 2),
-                side = 2,
-                las = 2,
-                cex.axis = .9
-            )
-            abline(
-                h = private$.step_2$step_1$statistic_value,
-                v = private$.samples["50%"],
-                col = "#2c2c2c",
-                lty = 3
-            )
-            polygon(
-                x = c(private$.step_2$interpolation$x, private$.step_2$interpolation$x[order(private$.step_2$interpolation$x, decreasing = TRUE)]),
-                y = c(private$.ci[, "0%"], private$.ci[, "100%"][order(private$.ci[, "100%"], decreasing = TRUE)]),
-                col = "#bc8f8f52",
-                border = NA
-            )
-            polygon(
-                x = c(private$.step_2$interpolation$x, private$.step_2$interpolation$x[order(private$.step_2$interpolation$x, decreasing = TRUE)]),
-                y = c(private$.ci[, self$lower_ci_string], private$.ci[, self$upper_ci_string][order(private$.ci[, self$upper_ci_string], decreasing = TRUE)]),
-                col = "#4683b48e",
-                border = NA
-            )
-            points(
-                col = "#00000070",
-                private$.step_2$step_1$range$partition,
-                private$.step_2$step_1$statistics,
-                pch = 19,
-                cex = .8
-            )
-            lines(
-                private$.step_2$interpolation$x,
-                private$.step_2$interpolation$fitted,
-                col = "#000000ad",
-                lwd = 2
+        plot = function(save = FALSE, path = NULL, width = 14, height = 10, ...) {
+            # Data confidence bands.
+            data_bands = data.frame(
+                x = rep(private$.step_2$interpolation$x, 2),
+                y = rep(private$.step_2$interpolation$fitted, 2),
+                lower = as.numeric(private$.ci[, c("0%", "2.5%")]),
+                upper = as.numeric(private$.ci[, c("100%", "97.5%")]),
+                ci = as.factor(sort(rep(c(
+                    paste0("0% - 100%", " (", private$.samples["100%"] - private$.samples["0%"], " sample sizes)"),
+                    paste0("2.5% - 97.5%", " (", private$.samples["97.5%"] - private$.samples["2.5%"], " sample sizes)")
+                ), nrow(private$.ci))))
             )
 
-            # Display CI for current statistic value.
-            segments(
-                x0 = c(private$.samples[self$lower_ci_string], private$.samples[self$upper_ci_string]),
-                y0 = c(min(private$.ci), min(private$.ci)) + 0.0,
-                x1 = c(private$.samples[self$lower_ci_string], private$.samples[self$upper_ci_string]),
-                y1 = c(private$.step_2$step_1$statistic_value, private$.step_2$step_1$statistic_value),
-                col = "#1c5b8f",
-                lty = 3,
-                lwd = 2
-            )
-            segments(
-                x0 = private$.samples[self$lower_ci_string],
-                y0 = private$.step_2$step_1$statistic_value,
-                x1 = private$.samples[self$upper_ci_string],
-                y1 = private$.step_2$step_1$statistic_value,
-                col = "#1c5b8f",
-                lty = 3,
-                lwd = 2
-            )
-            text(
-                c(private$.samples[self$lower_ci_string], private$.samples[self$upper_ci_string]),
-                c(min(private$.ci), min(private$.ci)) - 0.1,
-                c(private$.samples[self$lower_ci_string], private$.samples[self$upper_ci_string]),
-                col = "#000000",
-                font = 2,
-                cex = .9,
-                srt = 90,
-                offset = 0
-            )
-            legend(
-                "topleft",
-                title = expression(bold("Bootstrapped splines")),
-                legend = c("0% to 100%", paste0(self$lower_ci_string, " to ", self$upper_ci_string)),
-                fill = c("#bc8f8f52", "#4683b455"),
-                density = c(NA, NA),
-                bty = "n",
-                border = c("#bc8f8f52", "#4683b48e"),
-                cex = 0.7
+            # Data segments for bands annotation.
+            data_segments <- data.frame(
+                x_start = c(private$.samples[c(2, 4)], private$.samples[2]),
+                x_end = c(private$.samples[c(2, 4)], private$.samples[4]),
+                y_start = c(rep(min(private$.ci), 2), private$.step_2$step_1$statistic_value),
+                y_end = rep(private$.step_2$step_1$statistic_value, 3)
             )
 
-            # Sample size of interest.
-            sample <- private$.samples["50%"]
+            # Data statistics values for recommended sample.
+            data_statistics_recommendation <- self$get_statistics(private$.samples["50%"])
 
-            # Bootstrapped statistics values for the sample size of interest.
-            boot_statistics <- self$get_statistics(sample)
+            # Fetch common plot settings.
+            .__PLOT_SETTINGS__ <- plot_settings()
 
-            # Median and confidence intervals.
-            boot_statistics_median <- quantile(boot_statistics, .5)
-            boot_statistics_ci <- quantile(boot_statistics, c(0.025, 0.975))
+            # Plot for the confidence bands.
+            plot_bands <- ggplot2::ggplot(data_bands, ggplot2::aes(x = x, y = y)) +
+                ggplot2::geom_ribbon(
+                    mapping = ggplot2::aes(
+                        ymin = lower,
+                        ymax = upper,
+                        fill = ci,
+                        alpha = ci
+                    )
+                ) +
+                ggplot2::geom_line(
+                    mapping = ggplot2::aes(x = x, y = y),
+                    size = 1,
+                    color = "#000000",
+                    linetype = "solid"
+                ) +
+                ggplot2::geom_hline(
+                    yintercept = private$.step_2$step_1$statistic_value,
+                    color = "#8b0000",
+                    linetype = "dotted",
+                    size = .65
+                ) +
+                ggplot2::scale_y_continuous(
+                    breaks = seq(0, 1, .1)
+                ) +
+                ggplot2::scale_x_continuous(
+                    breaks = private$.step_2$step_1$range$partition
+                ) +
+                ggplot2::scale_fill_manual(
+                    name = "Confidence Bands",
+                    values = c("#4d4d4d", "#3e78a7")
+                ) +
+                ggplot2::scale_alpha_manual(
+                    name = "Confidence Bands",
+                    values = c(0.2, 0.5)
+                ) +
+                ggplot2::labs(
+                    title = paste0("Bootstrapped Splines", " (", private$.boots, " runs)"),
+                    x = "Sample Size Range",
+                    y = "Statistic Value"
+                ) +
+                .__PLOT_SETTINGS__ +
+                ggplot2::theme(
+                    legend.position = c(0.01, 0.99),
+                    legend.justification = c(0, 1),
+                    legend.background = ggplot2::element_rect(fill = NA)
+                )
 
-            # Plot histogram of statistics.
-            hist(
-                boot_statistics,
-                col = "#00000023",
-                border = FALSE,
-                main = paste0("Sample: ", sample, " | ", "Percentile: ", 0.5 * 100, "th"),
-                xaxt = "n",
-                xlab = ""
-            )
-            title(
-                xlab = "Bootstrapped statistics",
-                line = 4.5,
-                cex.main = 1,
-                cex.lab = 1
-            )
-            axis(
-                side = 1,
-                at = round(seq(min(boot_statistics), max(boot_statistics), by = 0.001), 2),
-                line = 1.5,
-                las = 2,
-                cex.axis = .9
-            )
-            # Confidence intervals.
-            abline(v = boot_statistics_ci, col = "#5f5f5f", lty = 3)
-            mtext(round(boot_statistics_ci, 3), side = 1, at = boot_statistics_ci, col = "rosybrown", font = 2, line = 0.3, cex = .8)
-            # Median.
-            abline(v = boot_statistics_median, col = "#5f5f5f", lty = 3)
-            mtext(round(boot_statistics_median, 3), side = 1, at = boot_statistics_median, col = "rosybrown",  font = 2, line = 0.3, cex = .9)
+            # Add annotations to the bands plot.
+            plot_bands <- plot_bands +
+                ggplot2::geom_segment(
+                    data = data_segments,
+                    ggplot2::aes(
+                        x = x_start,
+                        xend = x_end,
+                        y = y_start,
+                        yend = y_end
+                    ),
+                    linetype = "dashed",
+                    color = "#265881",
+                    size = .5
+                ) +
+                ggplot2::geom_segment(
+                    ggplot2::aes(
+                        x = private$.samples["50%"],
+                        xend = private$.samples["50%"],
+                        y = min(data_statistics_recommendation),
+                        yend = max(data_statistics_recommendation)
+                    ),
+                    linetype = "dashed",
+                    color = "#4d4d4d",
+                    size = .5
+                ) +
+                ggplot2::annotate(
+                    "label",
+                    x = data_segments[1, 1],
+                    y = min(private$.ci),
+                    label = data_segments[1, 1],
+                    color = "#265881"
+                ) +
+                ggplot2::annotate(
+                    "label",
+                    x = data_segments[2, 1],
+                    y = min(private$.ci),
+                    label = data_segments[2, 1],
+                    color = "#265881"
+                ) +
+                ggplot2::annotate(
+                    "label",
+                    x = private$.samples["50%"],
+                    y = min(data_statistics_recommendation),
+                    label = private$.samples["50%"],
+                    color = "#757575"
+                )
 
-            # Plot percentiles of bootstrapped statistics.
-            plot(
-                private$.step_2$step_1$range$sequence,
-                private$.ci[, "50%"],
-                type = "l",
-                lwd = 2,
-                main = paste0("Percentiles (", 0.5 * 100, "th)"),
-                ylab = paste0("Bootstrapped statistics"),
-                xlab = "",
-                xaxt = "n",
-                yaxt = "n"
-            )
-            title(
-                xlab = "Sample sizes",
-                line = 4.5,
-                cex.lab = 1,
-                cex.main = 1
-            )
-            axis(
-                side = 1,
-                at = floor(seq(min(private$.step_2$step_1$range$sequence), max(private$.step_2$step_1$range$sequence), length.out = 15)),
-                line = 1.5,
-                las = 2,
-                cex.axis = .9
-            )
-            axis(
-                side = 2,
-                at = round(seq(min(private$.ci[, "50%"]), max(private$.ci[, "50%"]), length.out = 10), 2),
-                las = 2,
-                cex.axis = .9
-            )
-            abline(h = private$.step_2$step_1$statistic_value, v = sample, lty = 2, col = "#5f5f5f")
-            mtext(sample, side = 1, at = sample, col = "darkgreen", font = 2, line = 0.3, cex = .9)
+            # Make and adjust density plots margins.
+            plot_density_lower <- self$density_plot(private$.samples["2.5%"]) & ggplot2::theme(plot.margin = ggplot2::margin(t = 15, r = 7.5, b = 0, l = 0))
+            plot_density_median <- self$density_plot(private$.samples["50%"]) & ggplot2::theme(plot.margin = ggplot2::margin(t = 15, r = 7.5, b = 0, l = 7.5))
+            plot_density_upper <- self$density_plot(private$.samples["97.5%"]) & ggplot2::theme(plot.margin = ggplot2::margin(t = 15, r = 0, b = 0, l = 7.5))
+
+            # Adjust margins for top plot.
+            plot_bands <- plot_bands & ggplot2::theme(plot.margin = ggplot2::margin(t = 0, r = 0, b = 0, l = 0))
+
+            # Prepare the main plot.
+            plot_step_3 <- plot_bands /
+                (plot_density_lower | plot_density_median | plot_density_upper) +
+                plot_layout(heights = c(1.5, 1))
+
+            # Save the plot.
+            if (save) {
+                if (is.null(path)) {
+                    # If no path is provided, create one.
+                    path <- paste0(getwd(), "/", "step-3", "_", gsub(":|\\s", "-", as.character(Sys.time()), perl = TRUE), ".pdf")
+                }
+
+                # Save the plot.
+                ggsave(path, plot = plot_step_3, width = width, height = height, ...)
+            } else {
+                # Show the plot.
+                plot(plot_step_3)
+            }
+
+            # Return the plot object silently.
+            invisible(plot_step_3)
         }
     ),
 
