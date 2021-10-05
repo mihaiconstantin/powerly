71d4cfb864322315165f3b3f1cc2318902465dbe
mihaiconstantin <mihai@mihaiconstantin.com>
2021-10-05 11:48:05 +0200

    Refactor: change GGM estimation to use `qgraph::EBICglasso`

--- R/GgmModel.R
+++ R/GgmModel.R
@@ -23,50 +23,18 @@ GgmModel <- R6::R6Class("GgmModel",
             return(data)
         },
 
-        # Todo: replace with `bootnet` estimators, also for the data generation.
-        estimate = function(data, n_lambda = 100, lambda_min_ratio = .01, gamma = 0.5) {
-            # Compute correlation matrix (i.e., inline with 'qgraph').
-            S <- cor(data)
+        estimate = function(data, gamma = 0.5) {
+            # Estimate network using `qgraph`.
+            network <- suppressMessages(suppressWarnings(
+                qgraph::EBICglasso(
+                    S = cov(data),
+                    n = nrow(data),
+                    gamma = gamma,
+                    verbose = FALSE
+                )
+            ))
 
-            # Compute the number of respondents.
-            n <- nrow(data)
-
-            # Compute the number of variables.
-            p <- nrow(S)
-
-            # Compute range of tunning parameter to be tried.
-            lambda_max = max(max(S - diag(p)), -min(S - diag(p)))
-            lambda_min = lambda_min_ratio * lambda_max
-            lambda = exp(seq(log(lambda_min), log(lambda_max), length = n_lambda))
-
-            # Compute 'glasso' path for the corresponding tunning parameters.
-            precision_matrices <- glasso::glassopath(S, lambda, trace = 0, penalize.diagonal = FALSE)
-
-            # Compute EBIC values for the 'glasso' path matrices.
-            ebic_values <- sapply(seq_along(lambda), function(i) {
-                # Extract the current precision matrix for easier access.
-                K <- precision_matrices$wi[, , i]
-
-                # Compute log likelihood.
-                L <- n / 2 * (log(det(K)) - sum(diag(K %*% S)))
-
-                # Compute the edge set.
-                E <- sum(K[lower.tri(K, diag = FALSE)] != 0)
-
-                # Compute the EBIC.
-                return(-2 * L + E * log(n) + 4 * E * gamma * log(p))
-            })
-
-            # Get precision matrix that minimizes the EBIC.
-            K <- precision_matrices$wi[, , which.min(ebic_values)]
-
-            # Standardize precision coefficients to get partial correlation coefficients.
-            W <- -cov2cor(K)
-
-            # Set diagonal to 0.
-            diag(W) <- 0
-
-            return(as.matrix(Matrix::forceSymmetric(W)))
+            return(network)
         },
 
         evaluate = function(true_parameters, estimated_parameters, measure) {
