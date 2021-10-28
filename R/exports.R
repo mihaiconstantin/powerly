49648aa1522a05d292e6f1a45ffa265b946772c1
mihaiconstantin <mihai@mihaiconstantin.com>
2021-10-28 11:49:56 +0200

    Refactor: add `roxygen` template for `generate_model` API

--- R/exports.R
+++ R/exports.R
@@ -157,25 +157,7 @@ validate <- function(method, replications = 3000, cores = NULL, backend_type = N
 }
 
 
-#' @title Generate true model parameters
-#'
-#' @description
-#' Generate matrices of true model parameters for the supported true models.
-#' These matrices are intended to passed to the `model_matrix` argument of
-#' [powerly::powerly()].
-#'
-#' @param type Character string representing the type of true model. Possible
-#' values are `"ggm"` (the default).
-#'
-#' @param ... Required arguments used for the generation of the true model. See
-#' the **True Models** section of [powerly::powerly()] for the arguments
-#' required for each true model.
-#'
-#' @return
-#' A matrix containing the model parameters.
-#'
-#' @seealso [powerly::powerly()], [powerly::validate()]
-#'
+#' @template generate_model
 #' @export
 generate_model <- function(type, ...) {
     # Create a model factory.
