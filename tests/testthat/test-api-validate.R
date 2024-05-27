51db681625555782ed353af5cee21c66f7284f5b
mihaiconstantin <mihai@mihaiconstantin.com>
2024-05-27 16:12:59 +0200

    Refactor(method): remove tests for method implementation

--- tests/testthat/test-api-validate.R
+++
@@ -1,8 +0,0 @@
-# Test 'validate()' public API.
-
-test_that("'validate()' fails on incorrect method object type", {
-    # Expect validate to fail on incorrect method object.
-    expect_error(validate(method = 1), .__ERRORS__$incorrect_type)
-    expect_error(validate(method = "method"), .__ERRORS__$incorrect_type)
-    expect_error(validate(method = StepOne$new()), .__ERRORS__$incorrect_type)
-})
