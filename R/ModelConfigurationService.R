d8f2b2abdb1ad4fb9bd1e21bac26b484f3fdebb1
mihaiconstantin <mihai@mihaiconstantin.com>
2024-10-10 17:36:53 +0200

    Feat: add `ModelConfigurationService` interface

---
+++ R/ModelConfigurationService.R
@@ -0,0 +1,70 @@
+#' @include Exception.R
+
+#' @title
+#' ModelConfigurationService
+#'
+#' @description
+#' This is an interface that defines methods for configuring the arguments
+#' passed to operations of a statistical model (i.e., public methods defined by
+#' the [`powerly::ModelService`] interface).
+#'
+#' @seealso
+#' [`powerly::ModelConfiguration`] and [`powerly::ModelService`].
+#'
+#' @export
+ModelConfigurationService <- R6::R6Class("ModelConfigurationService",
+    public = list(
+        #' @description
+        #' Create a new [`powerly::ModelConfigurationService`] object.
+        #'
+        #' @return
+        #' Instantiating this class will throw an error.
+        initialize = function() {
+            Exception$abstract_class_not_instantiable(self)
+        },
+
+        #' @description
+        #' Configure the arguments for the `specify` operation.
+        #'
+        #' @param ... Arguments for the `specify` operation.
+        #'
+        #' @return
+        #' This method returns void.
+        configure_specify = function(...) {
+            Exception$method_not_implemented()
+        },
+
+        #' @description
+        #' Configure the arguments for the `generate` operation.
+        #'
+        #' @param ... Arguments for the `generate` operation.
+        #'
+        #' @return
+        #' This method returns void.
+        configure_generate = function(...) {
+            Exception$method_not_implemented()
+        },
+
+        #' @description
+        #' Configure the arguments for the `estimate` operation.
+        #'
+        #' @param ... Arguments for the `estimate` operation.
+        #'
+        #' @return
+        #' This method returns void.
+        configure_estimate = function(...) {
+            Exception$method_not_implemented()
+        },
+
+        #' @description
+        #' Configure the arguments for the `evaluate` operation.
+        #'
+        #' @param ... Arguments for the `evaluate` operation.
+        #'
+        #' @return
+        #' This method returns void.
+        configure_evaluate = function(...) {
+            Exception$method_not_implemented()
+        }
+    )
+)
